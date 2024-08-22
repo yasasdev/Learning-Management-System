2f955a1db75cbdc0a8d19279c9379d35afc905c9
Yasas Lekamge <yasaslekamge@gmail.com>
2024-08-22 23:05:55 +0530

    Database connection file updated.

---
+++ config/dbconnection.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_SERVER',"localhost");
+define('DB_USERNAME',"root");
+define('DB_PASSWORD',"");
+define('DB_DATABASE',"db_learning");
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if(!$conn){
+    die("Connection Failed: ". mysqli_connect_error());
+}
+
+?>
